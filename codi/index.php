1939ba1b9c40e6ee92bf4d473cfd33510aac695a
dacomo dacomo <dacomo2122daw2@protonmail.com>
2022-09-27 02:16:33 +0200

    Commit 4

--- codi/index.php
+++ codi/index.php
@@ -1,5 +1,6 @@
 <?php
     echo "Hola món<br>";
     echo "Som el grup00<br>";
-    echo "Aquesta és l'activitat GP2F4A6.3"
+    echo "Aquesta és l'activitat GP2F4A6.3<br>";
+    echo "Estem treballant amb PHP VS Code Git VirtualBox i Vagrant";
 ?>
