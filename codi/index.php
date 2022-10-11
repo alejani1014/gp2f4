f6201369c909d83f032ef84c643879010cf73585
alejani1014 <alejani1014@gmail.com>
2022-10-11 17:02:04 +0200

    commit numero 4 sum,res,mul,div

--- codi/index.php
+++ codi/index.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
-<html lang="ca">
-	<head>
-		<meta charset="utf-8">
-		<title>
-			PÀGINA INICIAL DE L'APLICACIÓ CALCULADORA
-		</title>
-	</head>
-	<body>
-		<p><b>CALCUALDORA ARITMÈTICA</b></p>
-		<a href="suma.html">a) Suma de 2 operands</a>
-	</body>
-<html>
+	<html lang="ca">
+		<head>
+			<meta charset="utf-8">
+			<title>
+				PÀGINA INICIAL DE L'APLICACIÓ CALCULADORA
+			</title>
+		</head>
+		<body>
+			<p><b>CALCUALDORA ARITMÈTICA</b></p>
+			<a href="suma.html">a) Suma de 2 operands</a><br><br>
+			<a href="resta.html">b) Resta de 2 operands</a><br><br>
+			<a href="multiplicacio.html">c) Multiplicació de 2 operands</a><br><br>
+			<a href="divisio.html">d) Divisió de 2 operands</a>
+		</body>
+	<html>
